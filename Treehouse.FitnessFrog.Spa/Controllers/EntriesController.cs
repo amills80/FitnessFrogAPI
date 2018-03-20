b5e264858819630ad1cae8d809e42912d29cc5c9
Alan Mills <aelonrench@yahoo.com>
2018-03-20 16:42:50 -0400

    Add content negotiation, parameter binding, and add action method parameters to page functions.

---
+++ Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Models;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class EntriesController : ApiController
+    {
+
+        public IEnumerable<Entry> Get()
+        {
+            var activityBiking = new Activity() { Name = "Biking" };
+            return new List<Entry>()
+            {
+                new Entry(2018, 1 , 3, activityBiking, 10.0m),
+                new Entry(2017, 10 , 27, activityBiking, 5.0m)
+            };
+        }
+
+        public IEnumerable<Entry> Get(int id)
+        {
+            return null;
+        }
+
+        public void Post(Entry entry)
+        {
+            //return view();
+        }
+
+        public void Put(int id, Entry entry)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
--- Treehouse.FitnessFrog.Spa/README.md
+++ Treehouse.FitnessFrog.Spa/README.md
@@ -7,3 +7,19 @@
 * Adding an entry
 * Editing an entry
 * Deleting an entry
+
+
+## Endpoints
+
+* /api/entries GET , POST
+	* /api/entries/1 GET, PUT, DELETE
+* /api/activites GET
+* /api/intensisties GET
+
+# HTTP Status Codes
+
+* 200 OK
+* 201 Created
+* 204 No content
+* 400 Bad Request
+* 500 Int Server Error
