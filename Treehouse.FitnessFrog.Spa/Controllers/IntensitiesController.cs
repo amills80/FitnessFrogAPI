a34a9b0ba999242e21225ae6e3156b1716b06d34
Alan Mills <aelonrench@yahoo.com>
2018-03-21 16:11:17 -0400

    Add Activities and Intensities API endpoints.

---
+++ Treehouse.FitnessFrog.Spa/Controllers/IntensitiesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
+using Treehouse.FitnessFrog.Shared.Models;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class IntensitiesController : ApiController
+    {
+        //private IntensitiesRepository _intensitiesRepository = null;
+        //public IntensitiesController(IntensitiesRepository intensitiesRepository)
+        //{
+        //    _intensitiesRepository = intensitiesRepository;
+        //}
+
+        public IHttpActionResult Get()
+        {
+            var intensities = Enum.GetValues(typeof(Entry.IntensityLevel))
+                .Cast<Entry.IntensityLevel>()
+                .Select(li => new { id = (int)li, name = li.ToString() })
+                .ToList();
+            return Ok(intensities);
+        }
+    }
+}
