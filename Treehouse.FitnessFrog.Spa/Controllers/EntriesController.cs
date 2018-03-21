dd25f3bef3abf39d6699cccab98f9afcc7bc900c
Alan Mills <aelonrench@yahoo.com>
2018-03-20 21:30:55 -0400

    Build calls to repository methods. Configure JSON Serializer.

--- Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog.Spa/Controllers/EntriesController.cs
@@ -3,41 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
 using Treehouse.FitnessFrog.Shared.Models;
 
 namespace Treehouse.FitnessFrog.Spa.Controllers
 {
     public class EntriesController : ApiController
     {
+        private EntriesRepository _entriesRepository = null;
+        public EntriesController(EntriesRepository entriesRepository)
+        {
+            _entriesRepository = entriesRepository;
+        }
 
         public IEnumerable<Entry> Get()
         {
-            var activityBiking = new Activity() { Name = "Biking" };
-            return new List<Entry>()
-            {
-                new Entry(2018, 1 , 3, activityBiking, 10.0m),
-                new Entry(2017, 10 , 27, activityBiking, 5.0m)
-            };
+            return _entriesRepository.GetList();
         }
 
-        public IEnumerable<Entry> Get(int id)
+        public Entry Get(int id)
         {
-            return null;
+            return  _entriesRepository.Get(id);
         }
 
         public void Post(Entry entry)
         {
-            //return view();
+            _entriesRepository.Add(entry);
         }
 
         public void Put(int id, Entry entry)
         {
-
+            _entriesRepository.Update(entry);
         }
 
         public void Delete(int id)
         {
-
+            _entriesRepository.Delete(id);
         }
     }
 }
